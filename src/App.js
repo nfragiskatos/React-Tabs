1419c4f957d376e833f8ba2cd0a1eb9806a9afc9
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-24 06:32:20 -0700

    Setup async fetching of data from API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
+import React, { useState, useEffect } from 'react';
+import { FaAngleDoubleRight } from 'react-icons/fa';
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
-function App() {
-  return <h2>tabs project setup</h2>
+const url = 'https://course-api.com/react-tabs-project';
+function App () {
+	const fetchData = async () => {
+		const response = await fetch(url);
+		const data = await response.json();
+		console.log(data);
+	};
+	fetchData();
+	console.log('hello after data fetch');
+
+	return <h2>tabs project setup</h2>;
 }
 
-export default App
+export default App;
